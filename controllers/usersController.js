0cb59c2eff25ea2ba71755015ad6a826c58ccb49
Daniil Hovorun <hovorun.d@gmail.com>
2020-03-11 12:47:11 +0200

    request validation added

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,13 +8,13 @@ module.exports = {
 	},
 
 	newUser     : async (req, res, next) => {
-		const newUser = new User(req.body);
+		const newUser = new User(req.value.body);
 		const user = await newUser.save();
 		res.status(201).json(user);
 	},
 
 	getUser     : async (req, res, next) => {
-		const { userId } = req.params;
+		const { userId } = req.value.params;
 
 		const user = await User.findById(userId);
 		res.status(200).json(user);
--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,21 @@ const router = require('express-promise-router')();
 
 const UsersController = require('../controllers/usersController');
 
-router.route('/').get(UsersController.index).post(UsersController.newUser);
+const { validateParam, validateBody, schemas } = require('../helpers/routeHelpers');
+
+router
+	.route('/')
+	.get(UsersController.index)
+	.post(validateBody(schemas.userSchema), UsersController.newUser);
 
 //  /users/:id
 router
 	.route('/:userId')
-	.get(UsersController.getUser)
-	.put(UsersController.replaceUser)
+	.get(validateParam(schemas.idSchema, 'userId'), UsersController.getUser)
+	.put(
+		[ validateParam(schemas.idSchema, 'userId'), validateBody(schemas.userSchema) ],
+		UsersController.replaceUser
+	)
 	.patch(UsersController.updateUser);
 
 router.route('/:userId/cars').get(UsersController.getUserCars).post(UsersController.newUserCar);
